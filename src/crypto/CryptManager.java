f0a98c0145393eed0cdaec6e18689fd9c1444a1d
Sharkgrammer <danielkeanekelly@gmail.com>
2020-02-26 13:24:01 +0000

    CryptManager merged with master

--- src/crypto/CryptManager.java
+++ src/crypto/CryptManager.java
@@ -14,7 +14,6 @@ import java.util.List;
 public class CryptManager {
 
     //REF https://mkyong.com/java/java-asymmetric-cryptography-example/
-
     private String cipherInstance = "RSA/ECB/PKCS1Padding";
     private String keyInstance = "RSA";
     private KeyPair keys = null;
@@ -66,6 +65,31 @@ public class CryptManager {
         }
     }
 
+    public byte[] decryptMessagePub(byte[] msg, byte[] pub) {
+
+        try {
+            KeyFactory kf = KeyFactory.getInstance(keyInstance);
+            PublicKey pubKey = kf.generatePublic(new X509EncodedKeySpec(pub));;
+
+            return decryptMessagePub(msg, pubKey);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            return null;
+        }
+    }
+
+    public byte[] decryptMessagePub(byte[] msg, PublicKey pubKey) {
+        int lenBytes = returnMaxBytes((RSAKey) pubKey, false);
+
+        return decryptMessagePub(msg, pubKey, lenBytes);
+    }
+
+    public byte[] decryptMessagePub(byte[] msg, PublicKey pub, int maxLen) {
+        int len = msg.length;
+        List<byte[]> msgList = getMsgList(msg, len, maxLen);
+        return passThroughCipher(msgList, pub, len, Cipher.DECRYPT_MODE);
+    }
+
     public byte[] decryptMessagePriv(byte[] msg, byte[] priv) {
 
         try {
@@ -82,46 +106,14 @@ public class CryptManager {
     public byte[] decryptMessagePriv(byte[] msg, PrivateKey privKey) {
         int lenBytes = returnMaxBytes((RSAKey) privKey, false);
 
-        System.out.println("lenBytes: " + lenBytes);
-
         return decryptMessagePriv(msg, privKey, lenBytes);
     }
 
 
     public byte[] decryptMessagePriv(byte[] msg, PrivateKey priv, int maxLen) {
-        int len = msg.length, tempSize = 0;
-        List<byte[]> msgList = new ArrayList<>();
-
-        for (int lenCounter = 0; len > lenCounter; lenCounter += maxLen){
-            if (lenCounter + maxLen > len) {
-                tempSize += len - lenCounter;
-            }else{
-                tempSize += maxLen;
-            }
-
-            System.out.println("inMsg/bytelist: " + tempSize + " " + maxLen + " " + lenCounter + " " + len);
-
-            byte[] tempArr = Arrays.copyOfRange(msg, lenCounter, tempSize);
-            msgList.add(tempArr);
-        }
-
-        ByteArrayOutputStream byteStream = new ByteArrayOutputStream(len);
-        for (byte[] inMsg : msgList){
-            System.out.println("inMsg/outputstream: " + inMsg.length);
-            try {
-                Cipher cipher = Cipher.getInstance(cipherInstance);
-                cipher.init(Cipher.DECRYPT_MODE, priv);
-                System.out.println("CIPHER: " + cipher.doFinal(inMsg).length);
-                byteStream.write(cipher.doFinal(inMsg));
-            } catch (Exception e) {
-                System.out.println(e.toString());
-            }
-        }
-
-        byte[] resultBytes = byteStream.toByteArray();
-        System.out.println("resultBytes: " + resultBytes.length);
-
-        return resultBytes;
+        int len = msg.length;
+        List<byte[]> msgList = getMsgList(msg, len, maxLen);
+        return passThroughCipher(msgList, priv, len, Cipher.DECRYPT_MODE);
     }
 
     public byte[] encryptMessagePub(String msg, byte[] pub) {
@@ -149,9 +141,7 @@ public class CryptManager {
 
             System.out.println("encryptMessagePub/bytes: " + msg.length);
 
-            //REF https://stackoverflow.com/a/16268737/11480852
             int lenBytes = returnMaxBytes((RSAKey) pub, true);
-            System.out.println("lenBytes: " + lenBytes);
 
             return encryptMessagePub(msg, pub, lenBytes);
 
@@ -162,41 +152,54 @@ public class CryptManager {
     }
 
     private byte[] encryptMessagePub(byte[] msg, PublicKey pub, int maxLen) {
-        int len = msg.length, tempSize = 0;
-        List<byte[]> msgList = new ArrayList<>();
-
-        for (int lenCounter = 0; len > lenCounter; lenCounter += maxLen){
-            if (lenCounter + maxLen > len) {
-                tempSize += len - lenCounter;
-            }else{
-                tempSize += maxLen;
-            }
-
-            byte[] tempArr = Arrays.copyOfRange(msg, lenCounter, tempSize);
-            msgList.add(tempArr);
-        }
-
-        ByteArrayOutputStream byteStream = new ByteArrayOutputStream(len);
-        for (byte[] inMsg : msgList){
-            System.out.println("inMsg: " + inMsg.length);
-            System.out.println("inMsg: " + Arrays.toString(inMsg));
-            try {
-                Cipher cipher = Cipher.getInstance(cipherInstance);
-                cipher.init(Cipher.ENCRYPT_MODE, pub);
-                System.out.println("CIPHER: " + cipher.doFinal(inMsg).length);
-                byteStream.write(cipher.doFinal(inMsg));
-            } catch (Exception e) {
-                System.out.println(e.toString());
-            }
-        }
-
-        byte[] resultBytes = byteStream.toByteArray();
-        System.out.println("resultBytes: " + resultBytes.length);
-
-        return resultBytes;
+        int len = msg.length;
+        List<byte[]> msgList = getMsgList(msg, len, maxLen);
+        return passThroughCipher(msgList, pub, len, Cipher.ENCRYPT_MODE);
     }
 
-    //REF https://stackoverflow.com/questions/2922622/how-to-get-the-size-of-a-rsa-key-in-java#2922641
+    public byte[] encryptMessagePriv(String msg, byte[] priv) {
+        return encryptMessagePriv(msg.getBytes(), priv);
+    }
+
+    public byte[] encryptMessagePriv(byte[] msg, byte[] priv) {
+        try {
+
+            System.out.println("encryptMessagePriv/bytes: " + msg.length);
+
+            KeyFactory kf = KeyFactory.getInstance(keyInstance);
+            PrivateKey privKey = kf.generatePrivate(new PKCS8EncodedKeySpec(priv));
+
+            return encryptMessagePriv(msg, privKey);
+
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            return null;
+        }
+    }
+
+    public byte[] encryptMessagePriv(byte[] msg, PrivateKey priv) {
+        try {
+
+            System.out.println("encryptMessagePriv/bytes: " + msg.length);
+
+            int lenBytes = returnMaxBytes((RSAKey) priv, true);
+            System.out.println("lenBytes: " + lenBytes);
+
+            return encryptMessagePriv(msg, priv, lenBytes);
+
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            return null;
+        }
+    }
+
+    private byte[] encryptMessagePriv(byte[] msg, PrivateKey priv, int maxLen) {
+        int len = msg.length;
+        List<byte[]> msgList = getMsgList(msg, len, maxLen);
+        return passThroughCipher(msgList, priv, len, Cipher.ENCRYPT_MODE);
+    }
+
+    //REF https://stackoverflow.com/a/16268737/11480852
     private int returnMaxBytes(RSAKey key, boolean encrypt){
         return ((key.getModulus().bitLength() + 7) / 8) - (encrypt ? 11 : 0);
     }
@@ -210,31 +213,45 @@ public class CryptManager {
         return gen.getUserKey(size);
     }
 
-    public void run() {
-        try {
-            setKeys((PublicKey) null, null);
+    private List<byte[]> getMsgList(byte[] msg, int len, int maxLen){
+        List<byte[]> msgList = new ArrayList<>();
 
-            //devSaveKey();
+        System.out.println("lenBytes: " + maxLen);
 
-            System.out.println(keys.getPrivate());
-            System.out.println(keys.getPublic());
+        int tempSize = 0;
 
-            System.out.println(Arrays.toString(keys.getPrivate().getEncoded()));
-            System.out.println(Arrays.toString(keys.getPublic().getEncoded())); //*/
+        for (int lenCounter = 0; len > lenCounter; lenCounter += maxLen){
+            if (lenCounter + maxLen > len) {
+                tempSize += len - lenCounter;
+            }else{
+                tempSize += maxLen;
+            }
 
-            String message = "Pizza boop shark";
-            System.out.println(message);
-           /* byte[] msg = encryptMessagePriv(message);
-
-            System.out.println(msg);
-            System.out.println(Arrays.toString(msg));
-
-            String ans = decryptMessagePub(msg, keys.getPublic());
-            System.out.println(ans);*/
-
-        } catch (Exception e) {
-            System.err.println("Caught exception " + e.toString());
+            byte[] tempArr = Arrays.copyOfRange(msg, lenCounter, tempSize);
+            msgList.add(tempArr);
         }
+
+        return msgList;
+    }
+
+    private byte[] passThroughCipher(List<byte[]> msgList, Key key, int len, int mode){
+        ByteArrayOutputStream byteStream = new ByteArrayOutputStream(len);
+        for (byte[] inMsg : msgList){
+            System.out.println("inMsg/outputstream: " + inMsg.length);
+            try {
+                Cipher cipher = Cipher.getInstance(cipherInstance);
+                cipher.init(mode, key);
+                System.out.println("CIPHER: " + cipher.doFinal(inMsg).length);
+                byteStream.write(cipher.doFinal(inMsg));
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            }
+        }
+
+        byte[] resultBytes = byteStream.toByteArray();
+        System.out.println("resultBytes: " + resultBytes.length);
+
+        return resultBytes;
     }
 
 }
